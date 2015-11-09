aa348f98b1acd7e5b5a2f9570ca1f3faed565d27
Owen Hurford <owen@hurfo.com>
2015-11-09 22:43:45 +0000

    Add session and config file

--- server.js
+++ server.js
@@ -4,7 +4,10 @@
 
 var express = require('express'),
     path = require('path'),
-    http = require('http');
+    http = require('http'),
+    session = require('express-session');
+
+var config = require('./config');
 
 var app = express();
 
@@ -13,6 +16,21 @@ app.set('views', path.join(__dirname, 'template'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    key: '',
+    secret: config.secret,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: null,
+        secure: false
+    },
+    name: 'nacc-ses',
+    resave: false,
+    saveUninitialized: false,
+    proxy: null
+}));
 
 app.get('/', function (req, res) {
     res.render('index');
