b0a84535f6a69e156e50ad057e2fe33afc899a96
Owen Hurford <owen@hurfo.com>
2015-11-12 21:32:15 +0000

    Sessions last a week

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
     cookie: {
         path: '/',
         httpOnly: true,
-        maxAge: null,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         secure: false
     },
     name: 'nacc-ses',
