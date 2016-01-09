913195032bed952b1efcbf583cc2a4ec6571ffa1
Owen Hurford <owen@hurfo.com>
2016-01-09 20:15:31 +0000

    Add suspend and delete options in admin

--- server.js
+++ server.js
@@ -331,7 +331,32 @@ app.get('/admin/users', isActivatedUser, isAdminUser, function (req, res) {
     sqlConnection.query('SELECT * FROM `users` LIMIT ?, ?', [low, high], function (err, rows, fields) {
         if (err) throw err;
         res.render('admin-users', {rows: rows, low: low, high: high});
-    })
+    });
+});
+
+app.get('/admin/users/suspend', isActivatedUser, isAdminUser, function (req, res) {
+    sqlConnection.query("UPDATE `" + config.mysqlDatabase + "`.`users` SET `activated`=0 WHERE `idusers`=?", [req.query.idusers], function (err, result) {
+        res.redirect(req.headers.referer);
+    });
+});
+    
+app.get('/admin/users/unsuspend', isActivatedUser, isAdminUser, function (req, res) {
+    sqlConnection.query("UPDATE `" + config.mysqlDatabase + "`.`users` SET `activated`=2 WHERE `idusers`=?", [req.query.idusers], function (err, result) {
+        res.redirect(req.headers.referer);
+    });
+});
+
+app.get('/admin/users/delete', isActivatedUser, isAdminUser, function (req, res) {
+    sqlConnection.query("UPDATE `" + config.mysqlDatabase + "`.`users` SET `fname`='', `lname`='', `email`='', `activated`=0 WHERE `idusers`=?", [req.query.idusers], function (err, result) {
+        req.session.dest = req.headers.referer;
+        res.redirect('/admin/users/delete/google?idusers=' + req.query.idusers);
+    });
+});
+
+app.get('/admin/users/delete/google', isActivatedUser, isAdminUser, function (req, res) {
+    sqlConnection.query("DELETE FROM `" + config.mysqlDatabase + "`.`googleauth` WHERE `idusers`=?", [req.query.idusers], function (err, result) {
+        res.redirect('/continue');
+    });
 });
 
 
